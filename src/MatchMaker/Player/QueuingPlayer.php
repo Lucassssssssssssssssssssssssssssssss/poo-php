83c1f5c5fa57e7e0d23ae1b5f67e34d53ccb4217
Lucassssssssssssssssssssssssssssssss <lucasmidiere1@gmail.com>
2025-12-02 10:50:52 +0100

    Évoluez vers la composition

--- src/MatchMaker/Player/QueuingPlayer.php
+++ src/MatchMaker/Player/QueuingPlayer.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace App\MatchMaker\Player;
 
-class QueuingPlayer extends Player implements QueuingPlayerInterface
+class QueuingPlayer implements QueuingPlayerInterface
 {
+    /** @var PlayerInterface */
+    private $player;
+
     /** @var int */
-    protected $range = 1;
+    private $range = 1;
 
     public function __construct(PlayerInterface $player, $range = 1)
     {
-        parent::__construct($player->getName(), $player->getRatio());
+        $this->player = $player;
         $this->range = (int) $range;
+    }
+
+    public function getName(): string
+    {
+        return $this->player->getName();
+    }
+
+    public function getRatio(): float
+    {
+        return $this->player->getRatio();
+    }
+
+    public function updateRatioAgainst(AbstractPlayer $opponent, int $result)
+    {
+        $this->player->updateRatioAgainst($opponent, $result);
     }
 
     public function getRange(): int
